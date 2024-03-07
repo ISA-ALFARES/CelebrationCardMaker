3b270239bc09c3e006fb5779b0d6d4c83b38ca0c
isa <isa430752@gmail.com>
2024-03-07 23:37:20 +0300

    View design and some modifications

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// تحقق مما إذا كان الاسم صالحًا
+// Check if the name is valid
 function isValidName(name) {
-    var regex = /^[\u0600-\u06FF\s\u0020a-zA-Z]+$/; // يسمح فقط بالحروف العربية والإنجليزية والمسافات
+    var regex = /^[\u0600-\u06FF\s\u0020a-zA-Z]+$/; // Allows only Arabic and English letters and spaces
     return regex.test(name);
 }
 
-// طباعة اسم المستخدم وعرضه على البطاقة
+// Print the user's name and display it on the card
 function printUserName() {
     var userName = document.getElementById('name').value.trim();
 
+    // If the entered name is not valid, show an alert
     if (!isValidName(userName)) {
-        alert("الرجاء إدخال اسم صالح يحتوي فقط على الحروف العربية والإنجليزية والمسافات.");
+        alert("Please enter a valid name containing only Arabic and English letters and spaces.");
         return;
     }
 
+    // Get the selected choice
     var selectedChoice = document.querySelector('input[name="choice"]:checked').value;
 
-    // إنشاء عنوان URL مع المعلمات
+    // Create a URL with parameters
     var url = 'card-view.html?name=' + encodeURIComponent(userName) + '&choice=' + encodeURIComponent(selectedChoice);
 
-    // فتح العنوان URL في نافذة جديدة
+    // Open the URL in a new window
     window.open(url, '_blank');
 }
